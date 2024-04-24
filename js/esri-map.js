b476959851ca7484a235dbf05f8f2dc6aeab21ce
andrei-mendez <160060377+andrei-mendez@users.noreply.github.com>
2024-04-24 11:48:15 -0500

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -6,14 +6,14 @@ WildRydes.map = WildRydes.map || {};
 (function esriMapScopeWrapper($) {
     var wrMap = WildRydes.map;
 
-    wrMap.animate = function animate(origin, dest, callback) {          //  TODO moved
+    wrMap.animate = function animate(origin, dest, callback) {          
         let tick = 0;
         let id = null;
         const unicorn = WildRydes.unicorn;
 
-        let latlng = unicorn.getLatLng();
-        let latInc = (dest.latitude - latlng.lat) / 100;
-        let lngInc = (dest.longitude - latlng.lng) / 100;
+        let latlng = origin; // Set the starting point as the origin
+        let latInc = (dest.latitude - latlng.latitude) / 100; // Calculate latitude increment
+        let lngInc = (dest.longitude - latlng.longitude) / 100; // Calculate longitude increment
 
         clearInterval(id);
         id = setInterval(frame, 5);
@@ -23,13 +23,13 @@ WildRydes.map = WildRydes.map || {};
                 callback();
             } else {
                 tick++;
-                latlng = {lat: latlng.lat +  latInc, lng: latlng.lng +  lngInc};
+                latlng = {latitude: latlng.latitude +  latInc, longitude: latlng.longitude +  lngInc};
                 unicorn.setLatLng(latlng);
             }
         }
-    }
+    };
 
-    wrMap.unsetLocation = function unsetLocation() {		//	TODO moved
+    wrMap.unsetLocation = function unsetLocation() {		
         if (WildRydes.marker)
             WildRydes.marker.remove();
     };
@@ -37,8 +37,7 @@ WildRydes.map = WildRydes.map || {};
    // Function to retrieve weather data from OpenWeatherMap API
     function getWeatherData(latitude, longitude) {
         var apiKey = '7047dfe3a8d8d2e26bbb76e0c853b8e5';
-        var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
-
+        var apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
         // Make AJAX request to OpenWeatherMap API
         $.ajax({
             url: apiUrl,
@@ -75,4 +74,25 @@ WildRydes.map = WildRydes.map || {};
         }
     }
 
+    // Function to get user's current location using HTML5 Geolocation API
+    function getUserLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                
+                // Call the function to retrieve weather data with obtained coordinates
+                getWeatherData(latitude, longitude);
+            }, function(error) {
+                console.error('Error getting user location:', error);
+            });
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    }
+
+    // Call the function to get user's location and weather data when the page loads
+    getUserLocation();
+
 }(jQuery));
+
